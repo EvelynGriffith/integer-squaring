9236b8903aa30bfdbe55578dc1ab01a178e2480e
EvelynGriffith <70599987+EvelynGriffith@users.noreply.github.com>
2021-09-09 16:33:29 -0400

    adding test case

--- square/tests/test_square.py
+++ square/tests/test_square.py
@@ -49,6 +49,18 @@ def test_compute_square_iterative_for_loop():
     square_list = main.compute_square_iterative(number_list, square_function)
     assert square_list == [72 * 72, 29 * 29, 61 * 61, 42 * 42, 44 * 44]
 
-
-# TODO: Add a test case for confirming the correctness of the compute_square_iterative function
+# Add a test case for confirming the correctness of the compute_square_iterative function
 # when it is using the square_function as main.compute_square_for
+
+def test_compute_square_iterative_while_loop():
+    """Confirm that the for loop calculates squares correctly for negatives and positives in loop."""
+    number_list = """-72
+        29
+        61
+        -42
+        44"""
+    square_function = main.compute_square_for
+    square_list = main.compute_square_iterative(number_list, square_function)
+    assert square_list == [72 * 72, 29 * 29, 61 * 61, 42 * 42, 44 * 44]
+
+
